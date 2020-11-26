dacb47a0348c15fa6626c18d3fb4feb76d0ab065
James Pemantell <pbin2au@gmail.com>
2020-11-25 19:05:54 -0500

    Corrections, and error handling.

--- app/routes.py
+++ app/routes.py
@@ -26,11 +26,11 @@ def index():
     ]
     return render_template('index.html', title='The Homepage of Jamie', fortune_text=fortune_text, albums=albums)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}',format(
+        flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
-    return render_template('login.html', title="Sign In", form=form)
+    return render_template('login.html', title='Sign In', form=form)
